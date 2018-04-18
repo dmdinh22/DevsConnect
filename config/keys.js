e26646885105989e925039f55624c4f93d139d0c
David Dinh <dmdinh22@gmail.com>
2018-04-18 00:28:31 -0700

    connected to mongoDB

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+        'mongodb://batman:ironmansucks@ds247569.mlab.com:47569/devsconnect'
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// db config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongoDB via mongoose
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected.'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
