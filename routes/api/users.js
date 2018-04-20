5d210000b4d5df3373e64db7fa5003a06f9ecf39
David Dinh <dmdinh22@gmail.com>
2018-04-20 01:09:52 -0700

    started on validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// load input validation
+const validateRegisterInput = require('../../validation/register');
+
 // load user model
 const User = require('../../models/User');
 
---
+++ validation/is-empty.js
@@ -0,0 +1,7 @@
+const isEmpty = val =>
+    val === undefined ||
+    val === null ||
+    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'string' && value.trim().length === 0);
+
+module.exports = isEmpty;
