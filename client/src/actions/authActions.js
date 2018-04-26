1c204a431d7a1bfb83cb47aec2ebcbfdec01a0b8
David Dinh <dmdinh22@gmail.com>
2018-04-25 21:50:22 -0700

    logout and redirects when user is logged in

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -52,3 +52,15 @@ export const setCurrentUser = decoded => {
 		payload: decoded
 	};
 };
+
+// user logout
+export const logoutUser = () => dispatch => {
+	// remove token from localStorage
+	localStorage.removeItem('jwtToken');
+
+	// remove auth header for future requests
+	setAuthToken(false);
+
+	// set current user to {} which will set isAuthenticated to false
+	dispatch(setCurrentUser({}));
+};
