81c968b6a91baef994251e7fca8974ef7ffb45df
David Dinh <dmdinh22@gmail.com>
2018-04-27 00:42:11 -0700

    handle user without profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
@@ -20,7 +21,21 @@ class Dashboard extends Component {
 		if (profile === null || loading) {
 			dashboardContent = <Spinner />;
 		} else {
-			dashboardContent = <h1>Hello World</h1>;
+			// check if logged in user has profile data
+			if (Object.keys(profile).length > 0) {
+				dashboardContent = <h4>TODO: DISPLAY PROFILE</h4>;
+			} else {
+				// user is logged in but does not have profile
+				dashboardContent = (
+					<div>
+						<p className="lead text-muted">Welcome {user.name}</p>
+						<p> You have not set up a profile, please add some info.</p>
+						<Link to="/create-profile" className="btn btn-lg btn-info">
+							Create Profile
+						</Link>
+					</div>
+				);
+			}
 		}
 
 		return (
