74957901071a061d1503061ec39babe45da2bc65
David Dinh <dmdinh22@gmail.com>
2018-04-29 01:47:14 -0700

    added profileactions component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
+import ProfileActions from './ProfileActions';
 
 class Dashboard extends Component {
 	// lifecycle method
@@ -23,7 +24,14 @@ class Dashboard extends Component {
 		} else {
 			// check if logged in user has profile data
 			if (Object.keys(profile).length > 0) {
-				dashboardContent = <h4>TODO: DISPLAY PROFILE</h4>;
+				dashboardContent = (
+					<div>
+						<p className="lead text-muted">
+							Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
+						</p>
+						<ProfileActions />
+					</div>
+				);
 			} else {
 				// user is logged in but does not have profile
 				dashboardContent = (
