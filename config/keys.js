31f143520b237c609ff8b0870a1a98ed7d5ce63d
David Dinh <dmdinh22@gmail.com>
2018-05-02 00:55:49 -0700

    split dev and prod envs

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-	mongoURI: 'mongodb://batman:ironmansucks@ds247569.mlab.com:47569/devsconnect',
-	secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./keys_prod');
+} else {
+	module.exports = require('./keys_dev');
+}
