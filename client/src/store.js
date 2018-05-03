210d8f75e8b62876a309cd7711ccea066cb0f694
David Dinh <dmdinh22@gmail.com>
2018-05-02 23:36:16 -0700

    refactor to not include redux extension in prod

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
@@ -11,9 +11,9 @@ const store = createStore(
 	rootReducer,
 	initialState,
 	composeWithDevTools(
-		applyMiddleware(...middleware),
+		applyMiddleware(...middleware)
 		// setup for chrome ext
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	)
 );
 
